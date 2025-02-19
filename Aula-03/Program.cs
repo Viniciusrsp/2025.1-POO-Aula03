0afcfc649f7afdf89c64c12f3d724edb879e4af2
Vinirsp <Viniciusrsp7@gmail.com>
2025-02-19 14:26:10 +0000

    gestão de volume

--- Aula-03/Program.cs
+++ Aula-03/Program.cs
@@ -6,5 +6,8 @@ Console.WriteLine("Hello, World!");
 int Idade = 22;
 
 Televisao tvSala = new Televisao(55);
-
 Console.WriteLine($"O tamnho da tv é: {tvSala.Tamanho}");
+
+Console.WriteLine($"O volume da tv é: {tvSala.Volume}");
+tvSala.AumentarVolume();
+Console.WriteLine($"O volume da tv é: {tvSala.Volume}");
--- Aula-03/Televisao.cs
+++ Aula-03/Televisao.cs
@@ -2,32 +2,71 @@ namespace Aula_03;
 
 class Televisao
 {
-    // o metodo construtor possui o mesmo
-    // nome da classe. Ele não possui retorno (nem mesmo o void)
-    // Este metodo é executado sempre que uma instancia da classe
-    // é criada
-    // Por padrão, o C# cria um metodo contrutor vazio
-    // mas podems criar 
-    public  Televisao(float tamanho)
+    //O método construtor possui o mesmo nome da classe. 
+    // Ele não possui retorno (nem mesmo o void)
+    //Este método é executado sempre que uma instancia da classe
+    //é criada.
+    //Por padrão, o C# cria um método construtor publico vazio,
+    //mas podemos criar métodos construtores com outras
+    //visibilidades e recebendo parametros, se necessário. 
+    public Televisao(float tamanho)
     {
+        if (tamanho < TAMANHO_MINIMO || tamanho > TAMANHO_MAXIMO)
+        {
+            // Uma exeção interrompe o fluxo normal do sistema
+            throw new ArgumentOutOfRangeException($"O tamanho ({tamanho}) não é suportado.");
+        }
+
         Tamanho = tamanho;
+        Volume = VOLUME_PADRAO;
     }
 
+    // Optamos pela utilização da constantepara tornar o codigo mais legivel.
+    private const float TAMANHO_MINIMO = 22;
+    private const float TAMANHO_MAXIMO = 80;
+
+    private int VOLUME_MAXIMO = 100;
+    private int VOLUME_MINIMO = 0;
+    private int VOLUME_PADRAO = 10;
 
 
-    // Get: permite que seja executada a 
-    // Leitura atual da propriedade
-    // Set: permite que seja atribuido um 
-    // valor para a propriedade
+    //Get: permite que seja executada a 
+    //leitura do valor atual da propriedade
+    //Set: permite que seja atibuído um 
+    //valor para a propriedade
 
-    // classes, propriedades e metodos possuem modificadores de acesso
-    // public: visiveis a todo o projeto
-    // internal: visiveis somente no namespace
-    // protected: visiveis somente na classe e nas classes que herdam
-    // private: visiveis somaente na classe que foram criados
-    public float Tamanho { get; private set; }
+    //classes, propriedades e métodos possuem modificadores de acesso
+    //public: visiveis a todo o projeto
+    //internal: visiveis somente no namespace - padrão
+    //protected: visiveis somente na classe e nas classes que herdam
+    //private: visiveis somente na classe que foram criados
+    public float Tamanho { get; }
     public int Resolucao { get; set; }
-    public int Volume { get; set; }
+    public int Volume { get; private set; }
     public int Canal { get; set; }
     public bool Estado { get; set; }
+
+    public void AumentarVolume()
+    {
+        if (Volume < VOLUME_MAXIMO)
+        {
+            Volume++;
+        }
+        else
+        {
+            Console.WriteLine("Tv já está no volume máximo permitido");
+        }
+    }
+
+    public void DiminuirVolume()
+    {
+        if (Volume > VOLUME_MINIMO)
+        {
+            Volume--;
+        }
+        else
+        {
+            Console.WriteLine("Tv já está no volume minimo permitido");
+        }
+    }
 }
